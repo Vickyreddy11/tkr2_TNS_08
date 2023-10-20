4c815363cbe741780e77e9c3af7663263b38bc95
Vickyreddy11 <145426008+Vickyreddy11@users.noreply.github.com>
2023-10-20 10:13:27 +0530

    Add files via upload

---
+++ ece.java
@@ -0,0 +1,14 @@
+package tkrc;
+
+public class ece {
+	int a =12;
+	static int x =55;
+ public static void main (String[]args)
+ {
+	int n =99;
+	ece x1= new ece();
+System.out.println("local variable="+n);
+System.out.println("instance variable="+x1.a);
+System.out.println("static variable="+x);
+ }
+}
